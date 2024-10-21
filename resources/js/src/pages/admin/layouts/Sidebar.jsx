a42f8e8db228b83b4f8e005c0bbb0f62a5d309b1
jaimadhavan <jaipartha2001@gmail.com>
2024-10-21 18:22:52 +0530

    sidebarchanges

--- resources/js/src/pages/admin/layouts/Sidebar.jsx
+++ resources/js/src/pages/admin/layouts/Sidebar.jsx
@@ -8,7 +8,6 @@ import Dashboard from '../Dashboard';
 import Business from '../Business';
 import { BASE_URL } from "../../../App";
 
-// Import other necessary icons here...
 
 const Sidebar = () => {
     const [openSubmenu, setOpenSubmenu] = useState(null);
@@ -27,15 +26,15 @@ const Sidebar = () => {
                 <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li className="nav-item">
                         <a href={`${BASE_URL}`+"/#/dashboard"} className="nav-link align-middle px-0">
-                            <img src={facebook} alt="contact" className="img-fluid" /> 
+                            <img src={facebook} alt="contact" className="img-fluid" />
                             <span className="ms-1 d-none d-sm-inline">Home</span>
                         </a>
                     </li>
                     <li>
                         <a href={`${BASE_URL}`+"/#/dashboard"} onClick={() => toggleSubmenu('submenu1')} className="nav-link px-0 align-middle">
                             <img src={mail} alt="contact" className="img-fluid" />
-                            <span className="ms-1 d-none d-sm-inline">Dashboard</span>                          
-                            <img src={openSubmenu === 'submenu1' ? more : down} alt="Toggle submenu" className="ms-2" width="10"/>                           
+                            <span className="ms-1 d-none d-sm-inline">Dashboard</span>
+                            <img src={openSubmenu === 'submenu1' ? more : down} alt="Toggle submenu" className="ms-2" width="10"/>
                         </a>
                         <ul className={`collapse nav flex-column ms-1 ${openSubmenu === 'submenu1' ? 'show' : ''}`} id="submenu1">
                             <li className="w-100">
@@ -50,8 +49,8 @@ const Sidebar = () => {
                     <li>
                         <a href={`${BASE_URL}`+"/#/dashboard"} onClick={() => toggleSubmenu('submenu2')} className="nav-link px-0 align-middle">
                             <img src={mail} alt="contact" className="img-fluid" />
-                            <span className="ms-1 d-none d-sm-inline">Business</span>                          
-                            <img src={openSubmenu === 'submenu2' ? more : down} alt="Toggle submenu" className="ms-2"  data-toggle="collapse" data-target="#submenu1" width="10"/>                           
+                            <span className="ms-1 d-none d-sm-inline">Business</span>
+                            <img src={openSubmenu === 'submenu2' ? more : down} alt="Toggle submenu" className="ms-2"  data-toggle="collapse" data-target="#submenu1" width="10"/>
                         </a>
                         {/* ${openSubmenu === 'submenu2' ? 'show' : ''} */}
                         <ul className={`collapse nav flex-column ms-1`} id="submenu1">
@@ -65,7 +64,7 @@ const Sidebar = () => {
                     </li>
 
 
-                   
+
                     {/* Other main menu items... */}
                 </ul>
                 <hr />
